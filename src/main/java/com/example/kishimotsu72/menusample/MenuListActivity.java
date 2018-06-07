caf239c41ca7e43644bfe308833f97d60ade70e4
kishimotsu72 <kishimotsu72@gmail.com>
2018-06-07 10:11:15 +0900

    Initial commit

---
+++ src/main/java/com/example/kishimotsu72/menusample/MenuListActivity.java
@@ -0,0 +1,13 @@
+package com.example.kishimotsu72.menusample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_list);
+    }
+}
